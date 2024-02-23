34f53fd8a5c19368959a8820f364803edbbd2710
SerhiiQA <nabukve@gmail.com>
2024-02-23 16:59:56 +0200

    1

--- cypress/e2e/login_logout.cy.js
+++ cypress/e2e/login_logout.cy.js
@@ -1,7 +1,7 @@
 describe('Login/logout', () => {
     it('Login/logout',() => {
 // log in
-        cy.login('addchild@gmail.com', 'Addchild!1')
+        cy.login('info@chytozavryk.top', 'Adm1n123')
         cy.get('a.Button_button__aem_D:nth-child(1)').should('have.text', 'Адміністрування')
 // log_out
         cy.get('.Header_button__gdEXY').click()
