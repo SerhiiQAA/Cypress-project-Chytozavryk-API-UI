b5a26d0a3efdc4ee4694705f31e8a460fce34a85
SerhiiQA <nabukve@gmail.com>
2024-10-17 17:00:34 +0300

    new

--- cypress/e2e/Functional/wigwam.cy.js
+++ cypress/e2e/Functional/wigwam.cy.js
@@ -6,12 +6,12 @@ describe('Wigwam', () => {
 
         // cy.injectAxe();
         // cy.checkA11y();
-        
+
         const Wigwam_ = new Wigwam();
 
         Wigwam_.validateCookies()
         Wigwam_.validateSingInUser2()
-        Wigwam_. validateParentsPage()
+        Wigwam_.validateParentsPage()
         Wigwam_.validateWigwamPage()
     })
 })
